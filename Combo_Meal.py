0fa016c47df8fe5b41cac77e3279f39ccadb289e
Saikumar-ak <81463755+saikumar-aks@users.noreply.github.com>
2021-04-16 13:24:58 +0530

    Create Combo_Meal.py

---
+++ Combo_Meal.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+import os
+import sys
+
+def profit(b, s, c):
+    return ((b+s)-c)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        bsc = input().split()
+
+        b = int(bsc[0])
+
+        s = int(bsc[1])
+
+        c = int(bsc[2])
+
+        result = profit(b, s, c)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
